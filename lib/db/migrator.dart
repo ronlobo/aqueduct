19e0ef9e3e75162f4d88898b09654a9052c53a0b
Joe Conway <joe.conway@stablekernel.com>
2016-06-26 18:59:49 -0400

    Speccing out migration classes

--- lib/db/migrator.dart
+++ lib/db/migrator.dart
@@ -1,18 +1,29 @@
 part of aqueduct;
 
 abstract class Migrator {
+
+  DataModel targetDataModel;
+
   Future up();
   Future down();
 
   Future upData();
   Future downData();
 
-  void addTable(SchemaTable table) {
+  List<Map<String, dynamic>> operations = [];
 
+  void addTable(SchemaTable table) {
+    operations.add({
+      "op" : "table.add",
+      "table" : table.asSerializable()
+    });
   }
 
   void deleteTable(String tableName) {
-
+    operations.add({
+      "op" : "table.delete",
+      "name" : tableName
+    });
   }
 
   void renameTable(String existingTableName, String newName) {
@@ -24,11 +35,20 @@ abstract class Migrator {
   }
 
   void addColumn(String tableName, SchemaColumn column, dynamic initialValue) {
-
+    operations.add({
+      "op" : "column.add",
+      "tableName" : tableName,
+      "column" : column.asSerializable(),
+      "initialValue" : initialValue
+    });
   }
 
   void deleteColumn(String tableName, String columnName) {
-
+    operations.add({
+      "op" : "column.delete",
+      "tableName" : tableName,
+      "name" : columnName
+    });
   }
 
   void alterColumn(String tableName, SchemaColumn column, dynamic initialValue) {
@@ -36,14 +56,24 @@ abstract class Migrator {
   }
 
   void addIndex(String tableName, SchemaIndex index) {
-
+    operations.add({
+      "op" : "index.add",
+      "index" : index.asSerializable()
+    });
   }
 
   void renameIndex(String tableName, String existingIndexName, String newIndexName) {
-
+    operations.add({
+      "op" : "index.rename",
+      "sourceName" : existingIndexName,
+      "destinationName" : newIndexName
+    });
   }
 
   void deleteIndex(String tableName, String indexName) {
-
+    operations.add({
+      "op" : "index.delete",
+      "name" : indexName
+    });
   }
 }
