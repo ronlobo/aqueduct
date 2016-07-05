3d0291bd89996c33d290619eb3d2a639f57d278b
Sean Swezey <sean.swezey@stablekernel.com>
2016-07-05 16:04:37 -0400

    Add simple table and column commands

--- lib/db/postgresql/postgresql_schema_generator.dart
+++ lib/db/postgresql/postgresql_schema_generator.dart
@@ -25,6 +25,70 @@ class PostgreSQLSchemaGenerator extends SchemaGeneratorBackend {
     constraintCommands.addAll(constraints.map((c) => _foreignKeyConstraintForTableConstraint(table, c)).toList());
   }
 
+  void handleDeleteTableCommand(SchemaTable table) {
+    tableCommands.add("DROP TABLE ${table.name};");
+  }
+
+  void handleRenameTableCommand(SchemaTable table, String newName) {
+    tableCommands.add("ALTER TABLE ${table.name} RENAME TO ${newName};");
+  }
+
+  void handleAddColumnCommand(SchemaTable table, SchemaColumn column, dynamic initialValue) {
+    tableCommands.add("ALTER TABLE ${table.name} ADD COLUMN ${_columnStringForColumn(column)};");
+
+    if (column.relatedColumnName != null) {
+      constraintCommands.add(_foreignKeyConstraintForTableConstraint(table, column));
+    }
+  }
+
+  void handleDeleteColumnCommand(SchemaTable table, SchemaColumn column) {
+    tableCommands.add("ALTER TABLE ${table.name} DROP COLUMN ${_columnNameForColumn(column)} ${column.relatedColumnName != null ? "CASCADE" : "RESTRICT"};");
+  }
+
+  void handleRenameColumnCommand(SchemaTable table, SchemaColumn existingColumn, String newName) {
+    tableCommands.add("ALTER TABLE ${table.name} RENAME COLUMN ${_columnNameForColumn(existingColumn)} TO ${newName};");
+  }
+
+  void handleAlterColumnCommand(SchemaTable table, SchemaColumn existingColumn, SchemaColumn updatedColumn, dynamic initialValue) {
+    if (updatedColumn.isNullable != existingColumn.isNullable) {
+      if (updatedColumn.isNullable) {
+        constraintCommands.add("ALTER TABLE ${table.name} ALTER COLUMN ${_columnNameForColumn(existingColumn)} DROP NOT NULL;");
+      } else if (initialValue != null) {
+        constraintCommands.add("UPDATE ${table.name} SET ${_columnNameForColumn(existingColumn)}=${initialValue} WHERE ${_columnNameForColumn(existingColumn)} IS NULL;");
+        constraintCommands.add("ALTER TABLE ${table.name} ALTER COLUMN ${_columnNameForColumn(existingColumn)} SET NOT NULL;");
+      } else {
+        throw 'Initial value must be supplied for setting a column to not null';
+      }
+    }
+
+    if (updatedColumn.type != existingColumn.type) {
+      constraintCommands.add("ALTER TABLE ${table.name} ALTER COLUMN ${_columnNameForColumn(existingColumn)} SET DATA TYPE ${updatedColumn.type};");
+    }
+
+    if (updatedColumn.defaultValue != existingColumn.defaultValue) {
+      if (updatedColumn.defaultValue != null) {
+        constraintCommands.add("ALTER TABLE ${table.name} ALTER COLUMN ${_columnNameForColumn(existingColumn)} SET DEFAULT ${updatedColumn.defaultValue};");
+      } else {
+        constraintCommands.add("ALTER TABLE ${table.name} ALTER COLUMN ${_columnNameForColumn(existingColumn)} DROP DEFAULT;");
+      }
+    }
+
+    // TODO: unique
+    if (updatedColumn.isUnique != existingColumn.isUnique) {
+      throw 'UnsupportedOperation';
+//      if (updatedColumn.isUnique) {
+////        indexCommands.add("CREATE UNIQUE INDEX IF NOT EXISTS ${table.name}_${_columnNameForColumn(existingColumn)}_idx ON ${table.name} (${_columnNameForColumn(existingColumn)});");
+//        constraintCommands.add("ALTER TABLE ${table.name} ADD UNIQUE (${_columnNameForColumn(existingColumn)});");
+//      } else {
+//        constraintCommands.add("ALTER TABLE ${table.name} DROP CONSTRAINT ${table.name}_${_columnNameForColumn(existingColumn)}_key CASCADE;");
+//      }
+    }
+  }
+
+  void handleMoveColumnCommand(SchemaTable sourceTable, SchemaTable destinationTable, SchemaColumn column) {
+    throw 'UnsupportedOperation';
+  }
+
   String _foreignKeyConstraintForTableConstraint(SchemaTable sourceTable, SchemaColumn column) =>
       "ALTER TABLE ONLY ${sourceTable.name} ADD FOREIGN KEY (${_columnNameForColumn(column)}) "
           "REFERENCES ${column.relatedTableName} (${column.relatedColumnName}) "
