80a676ec6cece10e4b10405c03f7e80c7f62df2e
Sean Swezey <sean.swezey@stablekernel.com>
2016-07-05 16:04:37 -0400

    Add simple table and column commands

--- lib/db/schema_generator_backend.dart
+++ lib/db/schema_generator_backend.dart
@@ -24,4 +24,12 @@ abstract class SchemaGeneratorBackend {
   }
 
   void handleAddTableCommand(SchemaTable table);
+  void handleDeleteTableCommand(SchemaTable table);
+  void handleRenameTableCommand(SchemaTable existingTable, String newName);
+
+  void handleAddColumnCommand(SchemaTable table, SchemaColumn column, dynamic initialValue);
+  void handleDeleteColumnCommand(SchemaTable table, SchemaColumn column);
+  void handleRenameColumnCommand(SchemaTable table, SchemaColumn existingColumn, String newName);
+  void handleAlterColumnCommand(SchemaTable table, SchemaColumn existingColumn, SchemaColumn updatedColumn, dynamic initialValue);
+  void handleMoveColumnCommand(SchemaTable sourceTable, SchemaTable destinationTable, SchemaColumn column);
 }
